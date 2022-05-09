d20b3b76039225a190c40f60e3eb84d80c246d37
Alexander Tapia <alexandertapia@MacBook-Air-de-Alexander.local>
2022-05-09 07:16:53 -0700

    first commit

---
+++ AvtividadCalculadoraDePropinas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvtividadCalculadoraDePropinas
+//
+//  Created by Alexander Tapia on 06/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
